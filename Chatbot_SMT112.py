30b4189664cec33fafba9bc961eca67d70320d59
Venita <venita841114@gmail.com>
2019-11-05 19:49:52 +0800

    Links changes

--- Chatbot_SMT112.py
+++ Chatbot_SMT112.py
@@ -203,7 +203,7 @@ def flight(update, context):
     logger.info("user flight # %s", user_choice_num)
     update.message.reply_text(
         'Great!', 
-        reply_markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+        reply_markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True,resize_keyboard=True))
     return ENDING 
 
 def calculate_sum(user_dict):
@@ -233,30 +233,30 @@ def sendImg(update,cal_sum):
     user = update.message.from_user
     posList = ["https://media.giphy.com/media/UReWNqLOku35dpKIDb/giphy.gif",'https://media.giphy.com/media/iesygExkPilc0oeNZr/giphy.gif','https://media.giphy.com/media/XcMSqKXh4MDZ6Xg7TS/giphy.gif','https://media.giphy.com/media/l1KVcrdl7rJpFnY2s/giphy.gif','https://media.giphy.com/media/3o6UB7MOoxIfHet9PG/giphy.gif','https://media3.giphy.com/media/nWXvQqyGRJJtu/giphy.gif','https://cdn.dribbble.com/users/176039/screenshots/6944109/toogethr-wevoke-threetrees-01.gif','https://i.pinimg.com/originals/86/07/f3/8607f380ccf87e428cefcddd55ad4b7d.gif','https://i.pinimg.com/originals/b0/15/6f/b0156fdd707ab88d2a6ca71e758b16ab.gif','']
     negList = ['https://media.giphy.com/media/3oFzm9D3ofutq98QIU/giphy.gif','https://media.giphy.com/media/l3vRebbaRnNFVfvnW/giphy.gif','https://media.giphy.com/media/12XaepjzBlBUOY/giphy.gif','https://media.giphy.com/media/239S4COH5KHT2/giphy.gif','https://media.giphy.com/media/UBBRU1wwOwapG/giphy.gif','https://media.giphy.com/media/2jMDwajdZGUoy1dht2/giphy.gif','https://media.giphy.com/media/ZgSEzHSQbijCmhbS6F/giphy.gif','https://media.giphy.com/media/2tOrrbWzUfa8UoH1RO/giphy.gif','https://media.giphy.com/media/jpQJQxqANC450flu4i/giphy.gif','https://media.giphy.com/media/26gsm43ayV5GS5KDu/giphy.gif','https://media.giphy.com/media/pOgEzg2a8BOBEXZA7a/giphy.gif','https://media0.giphy.com/media/8clNSa9G7PRSp59Dx3/giphy.gif','https://media0.giphy.com/media/AS8jJdsFrs8c7YcyGC/source.gif']
+    negWebs = ['https://www.nationalgeographic.com/news/2012/1/120106-harp-seals-global-warming-sea-ice-science-environment/','https://inhabitat.com/photo-of-frail-polar-bear-illuminates-the-tragedy-unfolding-in-the-arctic/','http://content.time.com/time/health/article/0,8599,2095114,00.html','https://www.nationalgeographic.com/environment/2019/09/ipcc-report-climate-change-affecting-ocean-ice/','https://phys.org/news/2016-03-drought-recovery-rocky-mountain-forests.html','https://phys.org/news/2011-07-climate-change-induced-wildfires-yellowstone-forests.html','https://www.weadapt.org/knowledge-base/forests-and-climate-change/future-impacts-of-climate-change-on-forests','https://www.nationalgeographic.com/news/2018/02/polar-bears-starve-melting-sea-ice-global-warming-study-beaufort-sea-environment/']
+    posWebs = ['https://sciencing.com/the-importance-of-reducing-a-carbon-footprint-5229039.html','https://www.americanpowerandgas.com/10-ways-can-reduce-carbon-footprint-house/','https://www.conservation.org/carbon-offsets/?','https://www.saveonenergy.com/learning-center/energy-saving-tips/how-does-saving-energy-help-the-environment/','https://www.co2nsensus.com/how-can-i-save-the-planet','https://www.youtube.com/watch?time_continue=2&v=9pPsso2acew','https://www.theguardian.com/environment/2017/jan/19/how-to-reduce-carbon-footprint','https://www.carbonfootprint.com/carbonoffsetprojects.html','https://www.straitstimes.com/world/united-states/sixty-six-countries-vow-carbon-neutrality-by-2050-un']
     ranNum = 0
     if(cal_sum > 5):
         ranNum = random.randint(0,len(negList)-1)
-        update.message.reply_text("Oh no, your carbon footprint for today is above average...To find out how you can reduce it, check out this link: https://www.huffpost.com/entry/7-instant-ways-to-reduce-your-carbon-footprint_b_59321992e4b00573ab57a383")
+        update.message.reply_text("Oh no, your carbon footprint for today is *above average*...To find out how you can reduce it, check out this link:" + negWebs[random.randint(0,len(negWebs)-1)], parse_mode=ParseMode.MARKDOWN)
         imgUrl = negList[ranNum]
     else:
         ranNum = random.randint(0,len(posList)-1)
-        update.message.reply_text("Well done! Your carbon footprint today is within the average benchmark. Keep it up!")
+        update.message.reply_text("*Well done!* Your carbon footprint today is within the average benchmark. Keep it up!" + posWebs[random.randint(0,len(posWebs)-1)], parse_mode=ParseMode.MARKDOWN)
         imgUrl = posList[ranNum]
-    # print(ranNum)
-    # print(imgUrl)
     user.send_document(imgUrl)
 
 def ending(update, context):
+    reply_keyboard = [["/start"]]
     user = update.message.from_user
-    update.message.reply_text(
-        'The average carbon footprint per day is approximately *5kg*', parse_mode=ParseMode.MARKDOWN)
     cal_sum = calculate_sum(user_choice_dict[user.first_name])
-    update.message.reply_text('Thank you! Your carbon footprint today is: *' + str(round(cal_sum)) + '* kg.', parse_mode=ParseMode.MARKDOWN)
-    sendImg(update,cal_sum)
     update.message.reply_text(
-        'For more information, please visit: https://sciencing.com/the-importance-of-reducing-a-carbon-footprint-5229039.html'
-    )
-    update.message.reply_text('You may press /start to recalculate again!')
+        'Thank you! The average carbon footprint per day is approximately *5kg*. And your carbon footprint today is *' 
+        + str(round(cal_sum)) + 'kg.*'
+        , parse_mode=ParseMode.MARKDOWN
+        ,reply_markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True,resize_keyboard=True))
+    
+    sendImg(update,cal_sum)
     logger.info("user sum %s", str(round(cal_sum,2)))
     return ConversationHandler.END
 
@@ -276,7 +276,8 @@ def error(update, context):
 
 def main():
     # 1065787092:AAExGWnI-t3sirfBAtks_Hvb-6SeuhLBuw0
-    updater = Updater("1065787092:AAExGWnI-t3sirfBAtks_Hvb-6SeuhLBuw0", use_context=True)
+    # 733697750:AAECi30XQowgOibpPA0ZTHFOAu4UYYPRpA4
+    updater = Updater("733697750:AAECi30XQowgOibpPA0ZTHFOAu4UYYPRpA4", use_context=True)
     dp = updater.dispatcher
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
